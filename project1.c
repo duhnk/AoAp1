c0a42c007c9204b2591dfeb7e0d411f2132d079d
kcraguini <katherineraguini@yahoo.com>
2024-02-20 22:23:08 -0600

    modified:   project1.c

--- project1.c
+++ project1.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+This is the main file
+@author Zelig Garza
+@author Katherine Raguini
+*/
+
+//Make a merge sort and tweak it
+
+//Merge Sort (For larger information)
+
+
+
+//Quick Sort (For smaller information)
+
+void quicksort(int nums [], int size, int first, int last){
+	int i, j, pivot, tmp;
+	if(first < last){
+		pivot = first;
+		i = first;
+		j = last;
+		
+		while(i < j){
+			while(nums[i] < nums[pivot] && i < last)
+				i++;
+			while(nums[j] > nums[pivot])
+				j--;
+			if(i<j){
+				tmp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = tmp;
+			}
+		}
+		tmp = nums[pivot];
+		nums[pivot] = nums[j];
+		nums[j] = tmp;
+		quicksort(nums, size, first, j-1);
+		quicksort(nums, size, j+1, last);
+	}
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -13,12 +53,13 @@ int main(int argc, char *argv[])
 	float *left = NULL;
 	float *right = NULL;
 	float *startarr;
-       	startarr =(float*) malloc(size * sizeof(float));
-       	if (startarr==NULL){
-		printf("No memory available");
+	startarr =(float*) malloc(size * sizeof(float));
+
+	if (startarr==NULL) {
+	printf("No memory available");
 	}	
-	if (argc < 1)
-	{
+
+	if (argc < 1) {
 		printf("Not enough arguments");
 			return 1;
 	}
@@ -38,15 +79,16 @@ int main(int argc, char *argv[])
 			printf("inside array size\n");	
 			size *= 2;
 			/*realloc size of array to new capacity */
-			float *tmp = realloc(startarr,size );
+			startarr = (float*) realloc(startarr, size * sizeof(float));
 
-			if (tmp == NULL){
+			if (startarr == NULL){
 				printf("Error allocation failed to make tmp array!!!!!\n");
 				return -1;
 			}
-			startarr = tmp;
 		}
 	}
+
+	/*
 		printf("input numbers read: %d\n",num_read);
 		printf("the start array: %f\n",startarr[0]);
 		printf("the start array: %f\n",startarr[1]);
@@ -54,7 +96,8 @@ int main(int argc, char *argv[])
 		printf("the start array: %f\n",startarr[3]);
 		printf("the start array: %f\n",startarr[4]);
 		printf("the start array: %f\n",startarr[5]);
-		if (num_read <= 1) return;	
+	*/
+		if (num_read <= 1) return -1;	
 		int n1 = num_read/2; 
 		int n2 = num_read/2;
 		if(num_read%2 != 0) n2++;
@@ -72,13 +115,17 @@ int main(int argc, char *argv[])
 				v2[i-n1] = startarr[i];
 			}
 		}
+
+		/*
 		printf("the v1 array: %f\n",v1[0]);
 		printf("the v1 array: %f\n",v1[1]);
 		printf("the v1 array: %f\n",v1[2]);
 		printf("the v2 array: %f\n",v2[0]);
 		printf("the v2 array: %f\n",v2[1]);
 		printf("the v2 array: %f\n",v2[2]);
-		
+		*/
+
+
 	free(v1);
 	free(v2);
 	free(startarr);
