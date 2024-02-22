f80d84eaa9846e7b150eb0b089f92031519ec1d2
Zel <zelalegria5@gmail.com>
2024-02-22 17:55:22 -0600

    here is the new project file

--- project1.c
+++ project1.c
@@ -9,93 +9,18 @@ finding the most optimal buy and sell day for a list of prices
 @author Katherine Raguini
 */
 
-//Make a merge sort and tweak it
-
-//Merge Sort (For larger information)
-void merge(int nums [], int left, int mid, int right){
-	int i, j, k;
-	int n1 = mid - left - 1;
-	int n2 = right - mid;
-
-	int L[n1], R[n2];
-
-	// Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++)
-        L[i] = nums[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = nums[mid + 1 + j];
- 
-    // Merge the temp arrays back into arr[l..r]
-	//TO DO edit this to search through array
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-       //Write the comparision
-    }
- 
-    // Copy the remaining elements of L[],
-    // if there are any
-    while (i < n1) {
-        nums[k] = L[i];
-        i++;
-        k++;
-    }
- 
-    // Copy the remaining elements of R[],
-    // if there are any
-    while (j < n2) {
-        nums[k] = R[j];
-        j++;
-        k++;
-    }
-}
- 
-// l is for left index and r is right index of the
-// sub-array of arr to be sorted
-void mergeSort(int arr[], int l, int r)
-{
-    if (l < r) {
-        int m = l + (r - l) / 2;
- 
-        // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
- 
-        merge(arr, l, m, r);
-    }
-
-}
-
-//Quick Sort (For smaller information)
-
-void quicksort(int nums [], int size, int first, int last){
-	int i, j, pivot, tmp;
-	if(first < last){
-		pivot = first;
-		i = first;
-		j = last;
-		
-		while(i < j){
-			while(nums[i] < nums[pivot] && i < last)
-				i++;
-			while(nums[j] > nums[pivot])
-				j--;
-			if(i<j){
-				tmp = nums[i];
-				nums[i] = nums[j];
-				nums[j] = tmp;
-			}
-		}
-		tmp = nums[pivot];
-		nums[pivot] = nums[j];
-		nums[j] = tmp;
-		quicksort(nums, size, first, j-1);
-		quicksort(nums, size, j+1, last);
-	}
-}
-
 //Finds max in pointer
+float buysell(float L, float R)
+{
+
+	float buy = L, sell = R;
+	float profit = 0.0;
+	if(buy == sell)
+		return buy;
+	profit = sell - buy;
+return profit;	
+}
+
 float max(float *arr, int size)
 {
 	if (arr == NULL)
@@ -136,8 +61,90 @@ float min(float *arr, int size)
 
 	return curr;
 }
+float sort(float vec[], int n){
 
+	float profit1,profit2,profit3;
+	if (n <= 1) return;	
+	int n1 = n/2; 
+	int n2 = n/2;
 
+	//Adds more space to n2 if num_read is odd
+	if(n % 2 != 0) n2++;
+
+	//Allocates space for each pointer
+	float *v1 = malloc(n1*sizeof(float)); /* float v1[n1] array */
+	float *v2 = malloc(n2*sizeof(float)); /* float v2[n2] array */
+	int iminL = 0,imaxL = 0,iminR = 0,imaxR = 0;
+	//Divides array in two
+	int i = 0;
+	int min1 = vec[0];
+	int max1 = vec[0];
+	int min2 = vec[n1]; 
+	int max2 = vec[n1]; 
+	for (; i < n; i++)
+	{
+		if (i < n1)
+		{
+			v1[i] = vec[i];
+			if (min1 > vec[i])
+			{
+				min1 = vec[i];
+				iminL = i;
+			}
+			else if(max1 < vec[i])
+				imaxL = i;	
+		}
+		else
+		{
+			v2[i-n1] = vec[i];
+			if (min2 > vec[i])
+			{
+				max2 = vec[i];
+				iminR = i;
+			}
+			else if(max2 < vec[i])
+			{
+				max2 = vec[i];
+				imaxR = i;
+			}				
+		}
+	}
+
+	float minL = min(v1,n1);
+	float maxL = max(v1,n1);
+	float minR = min(v2,n2);
+	float maxR = max(v2,n2);
+	float *minAL = &minL;
+	float *maxAL = &maxL;
+	float *minAR = &minR;
+	float *maxAR = &maxR;
+	if(&minAR < &maxAR )
+		printf("TRUE\n");
+	else
+		printf("FALSE\n");
+	if(&maxAL < &maxAR)
+		printf("TRUE\n");
+
+	printf("minL = %.2f maxL = %.2f minR = %.2f maxR = %.2f\n",minL,maxL,minR,maxR);
+	printf("iminL = %d imaxL = %d iminR = %d imaxR = %d\n",iminL,imaxL,iminR,imaxR);
+	profit1 = buysell(minL,maxR);
+	profit2 = buysell(minL,maxL);	
+	profit3 = buysell(minR,maxR);
+	float sortL = sort(v1,n1);
+	float sortR = sort(v2,n2);
+	printf("profit1 = %.2f profit2 = %.2f profit3 = %.2f\n",profit1,profit2,profit3);
+	printf("sortL = %.2f sortR = %.2f \n",sortL,sortR);
+
+	if ( profit1 < profit3 && (&minAR < &maxAR))
+		return profit1;
+	else if (profit1 > profit3)
+	      return profit1;
+	else
+		return profit3;	
+
+	free(v1); free(v2);
+}
+/*
 float buysell(float L, float R)
 {
 	float buy = L, sell = R;
@@ -147,7 +154,7 @@ float buysell(float L, float R)
 	profit = sell - buy;
 return profit;	
 }
-
+*/
 //Main Driver
 int main(int argc, char *argv[])
 {
@@ -157,8 +164,6 @@ int main(int argc, char *argv[])
 
 	int size = 10;
 	int num_read = 0;
-	float *left = NULL;
-	float *right = NULL;
 	float *startarr;
 	startarr =(float*) malloc(size * sizeof(float));
 
@@ -196,7 +201,7 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-
+	float index = sort(startarr,num_read);	
 	/*
 		printf("input numbers read: %d\n",num_read);
 		printf("the start array: %f\n",startarr[0]);
@@ -208,19 +213,20 @@ int main(int argc, char *argv[])
 	*/
 
 
-	if (num_read <= 1) return -1;	
-	int n1 = num_read/2; 
-	int n2 = num_read/2;
+//	if (num_read <= 1) return -1;	
+//	int n1 = num_read/2; 
+/*	int n2 = num_read/2;
 
 	//Adds more space to n2 if num_read is odd
 	if(num_read % 2 != 0) n2++;
-
+*/
 	//Allocates space for each pointer
-	float *v1 = malloc(n1*sizeof(float)); /* float v1[n1] array */
-	float *v2 = malloc(n2*sizeof(float)); /* float v2[n2] array */
+
+//	float *v1 = malloc(n1*sizeof(float));  float v1[n1] array 
+//	float *v2 = malloc(n2*sizeof(float));  float v2[n2] array 
 
 	//Divides array in two
-	int i = 0;
+/*	int i = 0;
 	for (; i < num_read; i++)
 	{
 		if (i < n1)
@@ -232,7 +238,7 @@ int main(int argc, char *argv[])
 			v2[i-n1] = startarr[i];
 		}
 	}
-
+*/
 		/*
 		printf("the v1 array: %f\n",v1[0]);
 		printf("the v1 array: %f\n",v1[1]);
@@ -241,24 +247,23 @@ int main(int argc, char *argv[])
 		printf("the v2 array: %f\n",v2[1]);
 		printf("the v2 array: %f\n",v2[2]);
 		*/
-	float newmaxL = max(v1,n1);
+/*	float newmaxL = max(v1,n1);
 	float newmaxR = max(v2,n2);	
+*/
 /*
 	printf("max for left array %f\n", newmaxL);
 	printf("max for right array %f\n", newmaxR);
 */
 
-	float newminL = min(v1,n1);
-	float newminR = min(v2,n2);	
-	float profit = buysell(newminL,newmaxR);	
+//	float newminL = min(v1,n1);
+//	float newminR = min(v2,n2);	
+//	float profit = buysell(newminL,newmaxR);	
 /*
 	printf("min for left array %f\n", newminL);
 	printf("min for right array %f\n", newminR);
 */
-	printf("Profit for our buying and sell of %s dollars: %f\n",inputfile, profit);
+	printf("Profit for our buying and sell of %s dollars: %.2f\n",inputfile, index);
 
-	free(v1);
-	free(v2);
 	free(startarr);
 	fclose(senor);
 	return 0;
