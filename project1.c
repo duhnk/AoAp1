1915c93851bb1e922f05f9fb9fac8274cc7dbda5
kcraguini <katherineraguini@yahoo.com>
2024-02-21 23:26:48 -0600

    modified:   project1.c

--- project1.c
+++ project1.c
@@ -10,8 +10,67 @@ This is the main file
 //Make a merge sort and tweak it
 
 //Merge Sort (For larger information)
+void merge(int nums [], int left, int mid, int right){
+	int i, j, k;
+	int n1 = mid - left - 1;
+	int n2 = right - mid;
 
+	int L[n1], R[n2];
 
+	// Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = nums[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = nums[mid + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            nums[k] = L[i];
+            i++;
+        }
+        else {
+            nums[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        nums[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        nums[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+
+}
 
 //Quick Sort (For smaller information)
 
